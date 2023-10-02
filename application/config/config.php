72726144b2b7b42a91974918625c7842ac24dcb6
Karungg <miftahfadilah71@gmail.com>
2023-10-03 06:44:14 +0700

    [P03] Modify : Remove index.php from URL

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/webprogramming/semester3/ci3-pustaka-booking/index.php/';
+$config['base_url'] = 'http://localhost/ci3-pustaka-booking/';
 
 /*
 |--------------------------------------------------------------------------
