b362e4f0871d55d8b144ed66c844f64ed5feaa80
Karungg <miftahfadilah71@gmail.com>
2023-10-09 19:48:48 +0700

    [P06] Add : add user, book model

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,8 +58,10 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array();
-$autoload['libraries'] = array('form_validation');
+$autoload['libraries'] = array(
+	'form_validation', 'session',
+	'database'
+);
 
 /*
 | -------------------------------------------------------------------
@@ -90,7 +92,6 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array();
 $autoload['helper'] = array('url');
 
 /*
@@ -134,4 +135,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array();
+$autoload['model'] = array('ModelUser', 'ModelBuku');
